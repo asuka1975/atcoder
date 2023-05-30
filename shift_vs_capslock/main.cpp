810453c6d69ff6d32cdff64278f7c5a730589703
hungr <hungryfactory@icloud.com>
2023-05-31 00:01:06 +0900

    dash

---
+++ shift_vs_capslock/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+/*
+dp[i + 1] = 
+
+*/
+
+int main() {
+    int x, y, z;
+    std::string s;
+
+    std::cin >> x >> y >> z;
+    std::cin >> s;
+
+    std::vector<char> dp_op(600000);
+    std::vector<int>  dp_sc(600000);
+
+
+}
